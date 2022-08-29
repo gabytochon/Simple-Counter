7c010b4967586a2059f9e4e1bc0cd80954e5b2ae
gabytochon <gabrielatochon@gmail.com>
2022-08-29 02:04:39 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
 import React from "react";
+import PropTypes from "prop-types"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center d-flex justify-content-center">
+			<div className="bigCounter d-flex fs-1 gap-4 bg-black text-light w-25 justify-content-center">
+				<div className="clock" >
+					<i className="fa-regular fa-clock"></i>
+				</div>
+				<div className="Seis">
+					{props.numberSix % 10}
+				</div>
+				<div className="Cinco">
+					{props.numberFive % 10}
+				</div>
+				<div className="Cuatro">
+					{props.numberFour % 10}
+				</div>
+				<div className="Tres">
+					{props.numberThree % 10}
+				</div>
+				<div className="Dos">
+					{props.numberTwo % 10}
+				</div>
+				<div className="Uno">
+					{props.numberOne % 10}
+				</div>
+			</div>
 		</div>
 	);
 };
 
+Home.propTypes = {
+	numberOne: PropTypes.number,
+	numberTwo: PropTypes.number,
+	numberThree: PropTypes.number,
+	numberFour: PropTypes.number,
+	numberFive: PropTypes.number,
+	numberSix: PropTypes.number,
+}
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,15 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let counter =  0
+setInterval ( () => { 
+    const six = Math.floor (counter / 100000);
+    const five = Math.floor (counter / 10000);
+    const four = Math.floor (counter / 1000);
+    const three = Math.floor (counter / 100);
+    const two = Math.floor (counter / 10);
+    const one = Math.floor (counter / 1);
+    counter++;
+    ReactDOM.render(<Home numberOne={one} numberTwo={two} numberThree={three} numberFour={four} numberFive={five} numberSix={six}  />, document.querySelector("#app"));
+}, 1000)
